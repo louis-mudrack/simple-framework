eb257521c13d185b1a1c677dd67bc65cc7e5ac6c
Louis Mudrack <120463320+louis-mudrack@users.noreply.github.com>
2023-04-21 18:09:34 +0200

    Add files via upload

--- include/head.php
+++ include/head.php
@@ -5,20 +5,20 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php echo $text["title"]; ?></title>
-        <meta name="description" content="<?php echo $text["desc"]; ?>">
+        <title><?php echo $config['current-page']['title']; ?></title>
+        <meta name="description" content="<?php echo $config["current-page"]["desc"]; ?>">
         <meta name="author" content="<?php echo $text["client"]; ?>">
-        <meta property="og:title" content="<?php echo $text["title"]; ?>">
-        <meta property="og:description" content="<?php echo $text["desc"]; ?>">
+        <meta property="og:title" content="<?php echo $config["current-page"]["title"]; ?>">
+        <meta property="og:description" content="<?php echo $config["current-page"]["desc"]; ?>">
         <meta property="og:type" content="website">
         <meta property="og:url" content="<?php echo $config["url"]; ?>">
         <meta property="og:image" content="/images/social-branding.png">
-        <meta property="og:site_name" content="<?php echo $text["title"]; ?>">
+        <meta property="og:site_name" content="<?php echo $config["current-page"]["title"]; ?>">
         <meta name="thumbnail" content="/images/social-branding.png">
-        <meta name="theme-color" content="<?php echo $config["theme-color"]; ?>">
+        <meta name="theme-color" content="<?php echo $config['theme-color']; ?>">
         <meta name="format-detection" content="telephone=no">
-        <meta name="keywords" content="IMPORTANT KEYWORDS GO HERE">
-        <base href="<?php echo $config["base-url"]; ?>">
+        <meta name="keywords" content="Portfolio, HTML, CSS, JavaScript, JS, Programmieren, Berlin, Louis, Mudrack, 2023">
+        <base href="<?php echo $config["baseurl"]; ?>">
         <link rel="canonical" href="<?php echo $config["url"]; ?>">
         <!-- FAVICON -->
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
