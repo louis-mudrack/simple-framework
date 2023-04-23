3e100fbfb054ea94077b796c1f645c5318263c89
Louis Mudrack <120463320+louis-mudrack@users.noreply.github.com>
2023-04-23 21:42:12 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -2,33 +2,6 @@
 <?php require(SITE_ROOT."include/head.php"); ?>
     <?php require(SITE_ROOT."include/header.php"); ?>
         <main>
-            <div class="row">
-                <div class="col large-6">
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate dolores ullam laudantium tempora eos quisquam quibusdam est reiciendis quaerat modi fugit recusandae tenetur iste, et vel sequi delectus at tempore.</p>
-                </div>
-            </div>
+            
         </main>
         <?php require(SITE_ROOT."include/footer.php"); ?>
