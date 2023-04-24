dfb1155e0d8ff9c6b48bcb56f369e2a99ddf254a
Louis Mudrack <120463320+louis-mudrack@users.noreply.github.com>
2023-04-24 13:50:56 +0200

    Add files via upload

--- misc/mailing.php
+++ misc/mailing.php
@@ -6,11 +6,45 @@ if(isset($_POST["submit"])){
     $name = $_POST["name"];
     $business = $_POST["business"];
     $phone = $_POST["phone"];
+    $message = $_POST["message"];
     $callback = $_POST["callback"];
     $subject = "Form submission";
-    $message = $name . $business . $phone . $callback . " wrote the following:" . "\n\n" . $_POST["message"];
-    $headers = "From:" . $mail;
-    mail($to, $subject, $message, $headers);
+    $header  = "MIME-Version: 1.0\r\n";
+    $header .= "Content-type: text/html; charset=utf-8\r\n";
+    $header .= "X-Mailer: PHP ". phpversion();
+    $text = "
+    <html>
+        <body>
+            <table>
+                <tr>
+                    <td>Name:</td>
+                    <td>$name</td>
+                </tr>
+                <tr>
+                    <td>E-Mail:</td>
+                    <td>$mail</td>
+                </tr>
+                <tr>
+                    <td>Business:</td>
+                    <td>$business</td>
+                </tr>
+                <tr>
+                    <td>Phone:</td>
+                    <td>$phone</td>
+                </tr>
+                <tr>
+                    <td>Call back:</td>
+                    <td>$callback</td>
+                </tr>
+                <tr>
+                    <td>Message:</td>
+                    <td>$message</td>
+                </tr>
+            </table>
+        </body>
+    </html>
+    ";
+    mail($to, $subject, $text, $header);
     header("Location: ../contact/form-success.php");
     }
 ?>  
