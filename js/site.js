f58bbd0356bdc88c4d2a6b0910807ab986249162
Louis Mudrack <120463320+louis-mudrack@users.noreply.github.com>
2023-06-21 09:34:40 +0200

    Update site.js
    classes function for navi

--- js/site.js
+++ js/site.js
@@ -20,6 +20,12 @@ fetch("misc/navi.json")
                 if (item.external) {
                     a.setAttribute("target", "_blank");
                 }
+                if (item.class) {
+                    const classes = item.class.split(" ");
+                    classes.forEach((className) => {
+                        a.classList.add(className);
+                    });
+                }
                 li.appendChild(a);
                 li.classList.add(item.name.replace(/\s+/g, "-"));
                 nav.appendChild(li);
