3922c71f9b7493fcc014f8efdae1edacc086936b
Louis Mudrack <louis.mudrack@web.de>
2023-08-15 16:27:33 +0200

    updated missing mailing code

--- contact/index.php
+++ contact/index.php
@@ -3,15 +3,15 @@
         <div class="col large-8 large-prefix-2 large-suffix-2">
             <form method="post" action="../misc/mailing.php">
                 <label for="mail">Email:</label>
-                <input type="email" name="mail" id="mail">
+                <input type="email" name="mail" id="mail" required>
                 <label for="name">Name:</label>
-                <input type="text" name="name" id="name">
+                <input type="text" name="name" id="name" required>
                 <label for="business">Business:</label>
                 <input type="text" name="business" id="business">
                 <label for="phone">Phone:</label>
                 <input type="tel" name="phone" id="phone">
                 <label for="message">Message:</label>
-                <textarea name="message" id="message"></textarea>
+                <textarea name="message" id="message" required></textarea>
                 <label for="callback">Callback:</label>
                 <input type="checkbox" name="callback" id="callback" value="Yes">
                 <input type="submit" value="Submit">
--- misc/mailing.php
+++ misc/mailing.php
@@ -6,37 +6,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $errors = [];
 
     // Validate the email address
-    if (empty($_POST["mail"]) || !filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL)) {
+    if (isset($_POST["mail"]) &&!empty($_POST["mail"]) &&!filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL)) {
         // Invalid email address
         $errors['mail'] = "Please enter a valid email address.";
     }
 
     // Validate the name
-    if (empty($_POST["name"]) || strlen($_POST["name"]) > 100) {
+    if (isset($_POST["name"]) &&!empty($_POST["name"]) && strlen($_POST["name"]) > 100) {
         // Name too long
         $errors['name'] = "Your name is too long. Please limit to 100 characters.";
     }
 
     // Validate the business
-    if (empty($_POST["business"]) || strlen($_POST["business"]) > 100) {
+    if (isset($_POST["business"]) &&!empty($_POST["business"]) && strlen($_POST["business"]) > 100) {
         // Business too long
         $errors['business'] = "Your business is too long. Please limit to 100 characters.";
     }
 
     // Validate the phone number
-    if (empty($_POST["phone"]) || !preg_match('/^[0-9]+$/', $_POST["phone"])) {
+    if (isset($_POST["phone"]) &&!empty($_POST["phone"]) &&!preg_match('/^[0-9]+$/', $_POST["phone"])) {
         // Invalid phone number
-        $errors['phone'] = "Please enter a valid phone number.";
+        $errors['phone'] = "Please enter a valid phone number using numbers.";
     }
 
     // Validate the message
-    if (empty($_POST["message"]) || strlen($_POST["message"]) > 1000) {
+    if (isset($_POST["message"]) &&!empty($_POST["message"]) && strlen($_POST["message"]) > 1000) {
         // Message too long
         $errors['message'] = "Your message is too long. Please limit to 1000 characters.";
     }
 
     // Validate the callback
-    if (!empty($_POST["callback"]) && !filter_var($_POST["callback"], FILTER_VALIDATE_BOOLEAN)) {
+    if (isset($_POST["callback"]) &&!empty($_POST["callback"]) &&!filter_var($_POST["callback"], FILTER_VALIDATE_BOOLEAN)) {
         // Invalid callback value
         $errors['callback'] = "Please enter a valid callback value (true or false).";
     }
@@ -52,7 +52,52 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit;
     } else {
         // Send the email and redirect to form-success.php
-        //...
+        require_once($_SERVER["DOCUMENT_ROOT"]."/misc/site.config.php");
+        $to = $text["mail"];
+        $mail = $_POST["mail"];
+        $name = $_POST["name"];
+        $business = $_POST["business"];
+        $phone = $_POST["phone"];
+        $message = $_POST["message"];
+        $callback = $_POST["callback"];
+        $subject = "Form submission";
+        $header  = "MIME-Version: 1.0\r\n";
+        $header .= "Content-type: text/html; charset=utf-8\r\n";
+        $header .= "X-Mailer: PHP ". phpversion();
+        $text = "
+        <html>
+            <body>
+                <table>
+                    <tr>
+                        <td>Name:</td>
+                        <td>$name</td>
+                    </tr>
+                    <tr>
+                        <td>E-Mail:</td>
+                        <td>$mail</td>
+                    </tr>
+                    <tr>
+                        <td>Business:</td>
+                        <td>$business</td>
+                    </tr>
+                    <tr>
+                        <td>Phone:</td>
+                        <td>$phone</td>
+                    </tr>
+                    <tr>
+                        <td>Call back:</td>
+                        <td>$callback</td>
+                    </tr>
+                    <tr>
+                        <td>Message:</td>
+                        <td>$message</td>
+                    </tr>
+                </table>
+            </body>
+        </html>
+        ";
+        mail($to, $subject, $text, $header);
+        header("Location: ../contact/form-success.php");
     }
 }
 ?>
