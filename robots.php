1c6ce6ef43664c6d6a91f5d344ee29931d6d60dd
Louis Mudrack <louis.mudrack@web.de>
2024-06-12 16:57:29 +0200

    deleting unnecessary things

--- robots.php
+++ robots.php
@@ -1,12 +1,9 @@
 <?php
 header('Content-Type: text/plain');
 
-// Dynamic logic to determine what to disallow or allow
 echo "User-agent: *\n";
 echo "Disallow: /_essentials/\n";
 echo "Disallow: /_settings/\n";
 echo "Disallow: /_templates/\n";
-// Add more rules as needed
 
-// Dynamically set the sitemap URL
 echo "Sitemap: " . $_SERVER['HTTP_HOST'] . "/sitemap.xml\n";
