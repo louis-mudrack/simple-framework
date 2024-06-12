c5e0f9ad29a8e7df515ec29e84ad35e4154ba37f
louis-mudrack <louis.mudrack@web.de>
2024-06-12 14:38:24 +0200

    added lazyload and animations

--- js/site.js
+++ js/site.js
@@ -1,6 +1,8 @@
 import { Notification } from "./_essentials/Notification.js";
 import { initBackToTopButton } from "./_essentials/BackToTop.js";
 import { initFormValidation } from "./_essentials/FormValidation.js";
+import { LazyLoad } from "./_essentials/LazyLoad.js";
+import { LazyAnimation } from "./_essentials/LazyAnimation.js";
 
 initBackToTopButton();
 initFormValidation();
