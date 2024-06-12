f0e9dd475e5ceab83b0df01f5435fe0e56f0b94d
louis-mudrack <louis.mudrack@web.de>
2024-06-12 09:56:16 +0200

    changed site.contents to english

--- misc/site.contents.php
+++ misc/site.contents.php
@@ -6,66 +6,27 @@
     $navigation = array(
         "Home" => array(
             "url" => "/",
-            "title" => "Willkommen zu MoonyMystics | Handgemachter Schmuck",
-            "desc" => "Willkommen zu MoonyMystics! Wir bieten handgemachten Schmuck, der von der Natur und dem Universum inspiriert ist. Entdecken Sie unsere Kollektionen und finden Sie das perfekte Schmuckstück für sich oder einen geliebten Menschen.",
+            "title" => "Welcome to Your Business",
+            "desc" => "Welcome to Your Business. We offer a wide selection of ... and ...",
         ),
-        "Produkte" => array(
-            "url" => "/all-products",
-            "title" => "Alle Produkte | MoonyMystic",
-            "desc" => "Entdecken Sie alle unsere Produkte und finden Sie das perfekte Schmuckstück für sich oder einen geliebten Menschen. Von Ohrringen bis zu Halsketten haben wir für jeden etwas dabei.",
-        ),
-        "Über uns" => array(
-            "url" => "/about",
-            "title" => "Über uns | MoonyMystics",
-            "desc" => "Erfahren Sie mehr über MoonyMystics und unsere Mission, handgemachten Schmuck zu schaffen, der von der Natur und dem Universum inspiriert ist. Wir freuen uns darauf, von Ihnen zu hören!",
-        ),
-        "Kollektion" => array(
-            "url" => "/collection",
-            "title" => "Kollektionen | MoonyMystics",
-            "desc" => "Entdecken Sie unsere Kollektionen und finden Sie das perfekte Schmuckstück für sich oder einen geliebten Menschen. Jede Kollektion ist von der Natur und dem Universum inspiriert und handgefertigt.",
-        ),
-        "Bestellung abschließen" => array(
-            "url" => "/check-out",
-            "title" => "Bestellung abschließen | MoonyMystics",
-            "desc" => "Schließen Sie Ihre Bestellung ab und erhalten Sie Ihre handgefertigten Schmuckstücke so schnell wie möglich. Wir freuen uns darauf, Ihnen zu helfen!",
-            "navExclude" => true,
-        ),
-        "Login" => array(
-            "url" => "/login",
-            "title" => "Login | MoonyMystics",
-            "desc" => "Melden Sie sich an, um alle Bestellungen zu sehen und Produkte zu verwalten. Wir freuen uns darauf, Ihnen zu helfen!",
-            "navExclude" => true,
-        ),
-        "Dashboard" => array(
-            "url" => "/dashboard",
-            "title" => "Dashboard | MoonyMystics",
-            "desc" => "Verwalten Sie Ihre Bestellungen und Produkte im Dashboard. Wir freuen uns darauf, Ihnen zu helfen!",
-            "navExclude" => true,
-        ),
-        "Weitere Information" => array(
-            "url" => "/infos",
-            "title" => "Weitere Informationen bezüglich Lieferung und Bestellung | MoonyMystics",
-            "desc" => "Erfahren Sie mehr über Lieferung, Bestellung. Wir freuen uns darauf, Ihnen zu helfen!",
-            "navExclude" => true,
-        ),
-        "Kontakt" => array(
+        "Contact" => array(
             "url" => "/contact/index",
-            "title" => "Kontaktiere uns | MoonyMystics",
-            "desc" => "Haben Sie Fragen oder Anregungen? Kontaktieren Sie uns und wir helfen Ihnen gerne weiter. Wir freuen uns darauf, von Ihnen zu hören!",
+            "title" => "Contact us | Your Business",
+            "desc" => "Contact us for any questions or inquiries. We are happy to help you find the perfect piece of jewelry or answer any questions you may have.",
             "navExclude" => true,
             "footerNav" => true,
             "sub" => array(
-                "Formularbestätigung" => array(
+                "Form Confirmation" => array(
                     "url" => "/contact/form-success",
-                    "title" => "Formularbestätigung | MoonyMystics",
-                    "desc" => "Vielen Dank für Ihre Nachricht! Wir werden uns so schnell wie möglich bei Ihnen melden.",
+                    "title" => "Form Successfully send | Your Business",
+                    "desc" => "Thank you for contacting us. We will get back to you as soon as possible. In the meantime, feel free to browse our website and discover our products.",
                     "noIndex" => true,
                     "navExclude" => true,
                 ),
-                "Formularfehler" => array(
+                "Form Error" => array(
                     "url" => "/contact/form-error",
-                    "title" => "Formularfehler | MoonyMystics",
-                    "desc" => "Es gab ein Problem beim Senden Ihrer Nachricht. Bitte versuchen Sie es erneut oder kontaktieren Sie uns direkt per E-Mail oder Telefon. Wir entschuldigen uns für die Unannehmlichkeiten.",
+                    "title" => "Form Error | Your Business",
+                    "desc" => "There was a problem with your form submission. Please try again. If the problem persists, please contact us directly. We apologize for the inconvenience.",
                     "noIndex" => true,
                     "navExclude" => true,
                 )
